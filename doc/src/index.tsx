66ab9879bb3c5f1ebfeee00bd73a0c4dc65dad4c
Guo Yunhe <i@guoyunhe.me>
2019-12-29 15:01:26 +0200

    update config

--- doc/src/index.tsx
+++ doc/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "../src/index";
+import Hello from "../../src/index";
 
 const root = document.createElement("div");
 document.body.append(root);
